daff681e00c5edacb1e1a5a4680d7d1c207e1037
Alex Zajichek <alexzajichek@Alexs-MacBook-Pro.local>
2022-12-10 11:00:54 -0600

    Structure in place

--- R/reactable_aggregate_weighted_mean.R
+++ R/reactable_aggregate_weighted_mean.R
@@ -1,2 +1,15 @@
+#' Compute weighted averages in a reactable
+#'
+#' @param x A object
+#'
+#' @return Another object
+#' @export
+#'
+#' @examples
+#' reactable_aggregate_weighted_mean(5)
 reactable_aggregate_weighted_mean <-
-  function(x) x
+  function(x)
+  {
+    htmlwidgets::JS("")
+    x
+  }
---
+++ tests/testthat/test-reactable_aggregate_weighted_mean.R
@@ -0,0 +1,3 @@
+test_that("I get the value", {
+  expect_equal(reactable_aggregate_weighted_mean(5), 5)
+})
